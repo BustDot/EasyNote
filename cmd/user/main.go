c3db5a35ce685c5fa244a31298f4b9eec93bb14c
BustDot <Bust.dev@outlook.com>
2024-05-20 12:05:12 +0800

    feat: complete user module

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"easy_note/cmd/user/dal"
 	user "easy_note/kitex_gen/note/user/userservice"
-	"log"
+	bound "easy_note/pkg"
+	"easy_note/pkg/constants"
+	"easy_note/pkg/middleware"
+	tracer2 "easy_note/pkg/tracer"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
+	trace "github.com/kitex-contrib/tracer-opentracing"
+	"net"
 )
 
+func Init() {
+	tracer2.InitJaeger(constants.UserServiceName)
+	dal.Init()
+}
+
 func main() {
-	svr := user.NewServer(new(UserServiceImpl))
-
-	err := svr.Run()
-
+	r, err := etcd.NewEtcdRegistry([]string{constants.EtcdAddress})
 	if err != nil {
-		log.Println(err.Error())
+		panic(err)
+	}
+	ip, err := constants.GetOutBoundIP()
+	if err != nil {
+		panic(err)
+	}
+	addr, err := net.ResolveTCPAddr("tcp", ip+":8889")
+	if err != nil {
+		panic(err)
+	}
+	Init()
+	svr := user.NewServer(new(UserServiceImpl),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: constants.UserServiceName}), // server name
+		server.WithMiddleware(middleware.CommonMiddleware),                                             // middleware
+		server.WithMiddleware(middleware.ServerMiddleware),
+		server.WithServiceAddr(addr),                                       // address
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}), // limit
+		server.WithMuxTransport(),                                          // Multiplex
+		server.WithSuite(trace.NewDefaultServerSuite()),                    // tracer
+		server.WithBoundHandler(bound.NewCpuLimitHandler()),                // BoundHandler
+		server.WithRegistry(r),                                             // registry
+	)
+	err = svr.Run()
+	if err != nil {
+		klog.Fatal(err)
 	}
 }
